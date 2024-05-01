a064ff550e49e0ea5f3055e32dbbc34cef373f56
zxia15 <zxia15@login02.cm.cluster>
2024-05-01 13:37:15 -0400

    modified files

---
+++ executables/gpt2_finetune_forward_backbone.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -A danielk_gpu
+#SBATCH --partition a100
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=12:00:00
+#SBATCH --qos=qos_gpu
+#SBATCH --job-name="test_gpus"
+#SBATCH --output="gpt2_finetune_backbone_forward_alpaca.txt" # Path to store logs
+
+module load anaconda
+conda activate myenv # activate the Python environment
+
+# runs your code
+python -u ../src/gpt2_finetune_but_classification_backbone.py
